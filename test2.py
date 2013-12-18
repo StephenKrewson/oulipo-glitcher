d47b80616738538f52ea8de6079d0feeb44b8ec4
Stephen Krewson <stephen.krewson@gmail.com>
2013-12-18 18:20:48 -0500

    last desktop

--- test2.py
+++ test2.py
@@ -1,6 +1,16 @@
-import base64, binascii, sys
+import base64, binascii, sys, winsound, random
 from collections import defaultdict
 from os import walk, path
+import math as m
+from bisect import bisect_left
+
+# Get notes and frequencies in Hertz [37 - 5632.08 Hz]
+notes = ('A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#')
+freqs = []
+[freqs.append(37 * m.exp(p * m.log(2)/12)) for p in range(88)]
+
+def decToNote(d, array):
+    return d/255 * (len(array) - 1)
 
 def glitchFile(filename):
     d = defaultdict(lambda:0)
@@ -9,26 +19,17 @@ def glitchFile(filename):
     with open(filename, 'rb') as f:
         data = bytearray(f.read())
     for x in range(len(data)):
+        print(chr(data[x]))
         d[data[x]] += 1
         if data[x] == 7:
             bells += 1
             locations.append(x/len(data))
             if x % 7 == 0:
-                data.insert(x, 7)
+                data.insert(x, 90)
     with open(filename.split('.')[0] + 'MOD.' + filename.split('.')[1], 'wb') as target:
         target.write(data)
-    print(locations)
 for dirpath, dirnames, filenames in walk(path.abspath(sys.argv[1])):
     for i in filenames:
         files = path.join(dirpath, i)
         if files[-7:-4] != 'MOD':
-            glitchFile(files) 
-
-'''
-for key in d:
-    try:
-        print(chr(key), d[key])
-        howmany += 1
-    except:
-        UnicodeEncodeError
-'''
+            glitchFile(files)
