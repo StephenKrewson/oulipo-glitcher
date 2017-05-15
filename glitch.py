f9ef964afe7228997d34b03972ed7fbe63c96ff2
Stephen Krewson <stephen.krewson@gmail.com>
2017-05-15 19:50:56 -0400

    ok now skip bytes

--- glitch.py
+++ glitch.py
@@ -1,11 +1,20 @@
-# Stephen Krewson, 15 May 2017
+# Stephen Krewson, 15 May 2017 (created 2014, revised 2015)
 #
 # Oulipean Image Glitching
 #
-# Usage: python glitch.py <input-dir> <output-dir>
+# Usage: python3 glitch.py <input-imgs-dir> <output-imgs-dir>
+#
+# Supported filetypes: JPEG, PNG
 
 
+import imghdr
+from os import walk, path 
 import sys
+
+
+def glitch(img, type):
+	'''Oulipean glitch of binary data within JPEG or PNG'''
+	
 
 
 # Initialize the program; get params and check file
@@ -13,7 +22,25 @@ def main():
 
 	# there should only be three arguments
 	if (len(sys.argv) is not 3):
-		exit("Usage: python glitch.py <input-dir> <output-dir>")
+		exit("Usage: python3 glitch.py <input-imgs-dir> <output-imgs-dir>")
+		
+	# go through each file in <input-dir>; we use the absolute path
+	# so that ANY directory on the machine can be specified
+	for dirpath, dirnames, filenames in walk(path.abspath(sys.argv[1])):
+		for i in filenames:
+			file = path.join(dirpath, i)
+			
+			# figure out if this file is a jpeg or png (otherwise ignore)
+			imgtype = imghdr.what(file)
+			if imgtype not in ['jpeg','png']:
+				continue
+				
+			# assuming we have valid image...
+			print(file)
+			
+	
+	
+	
 	
 	print("hippo")
 
