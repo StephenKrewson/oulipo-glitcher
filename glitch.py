9863444bee5921b376d0ee8a83cd7f3b1fdb3f9b
Stephen Krewson <stephen.krewson@gmail.com>
2017-05-16 12:33:46 -0400

    lol getting crazy ANSI stuff

--- glitch.py
+++ glitch.py
@@ -7,30 +7,35 @@
 # Supported filetypes: JPEG, PNG
 
 
+from collections import defaultdict
 import imghdr
-from os import walk, path 
+from os import walk, path
+import re
 import sys
 
 
-def glitch(img, type):
+def glitch(img, type, lexicon):
 	'''Oulipean glitch of binary data within JPEG or PNG'''
-	
-	# first step is to open the file
+	# first step is to open the file and convert to byte array
 	with open(img,'rb') as f:
-		
-		# then convert to byte array
 		bytes = bytearray(f.read())
 	
 		# skip headers if it's a jpeg
 		if type is 'jpeg':
 			jpeg_start = skip_jpeg_header(bytes)
+			
 			print("starting byte", jpeg_start)
+			
+			index_chars(bytes[jpeg_start:])
+			
+			
 			
 			# now open up a .jpg file in out-dir, write headers to it
 		
 		else:
 			print("PNG not supported yet.")
-		
+
+			
 def skip_jpeg_header(byte_array):
 	'''skips to the start of the scan in a jpeg; input is byte array, see:
 	https://github.com/snorpey/glitch-canvas/blob/master/src/glitch/jpgHeaderLength.js
@@ -41,14 +46,60 @@ def skip_jpeg_header(byte_array):
 			start_scan = i + 2
 			break
 	return start_scan
+
+
+def index_chars(byte_array):
+	# counts for all utf-8 codepoints in the scan section of the image
+	d = defaultdict(lambda: 0)
+	
+	# list of tuples of (char, index) within the byte array
+	l = []
+	
+	# interpret as utf-8, ignoring errors
+	chars = byte_array.decode('utf-8', 'ignore')
+
+	for char in chars:
+		d[char] += 1
+		
+	
+	'''
+	for i in range(len(byte_array)):
+	
+		# try to convert to unicode
+		try:
+			char = byte_array[i].decode('utf-8')
+			d[char] += 1
+			
+			if re.match('[a-zA-Z]', char):
+				l.append((char, i))
+			
+		except UnicodeDecodeError:
+			pass'''
+	printHistogram(d)
+	return (d, l)
 	
 
+def printHistogram(d):                      # Function 2
+    '''Prints out chars by frequency'''
+    ordered = sorted(d, key=d.get, reverse=False)
+    max_val = d[ordered[-1]]                # Normalize max value to 50 chars
+    total = 0                               # 
+    for key in ordered:
+        total += d[key]
+        hist = key * round(d[key] * 50 / max_val)
+        print(key, hist)
+	
+	
 # Initialize the program; get params and check file
 def main():
 
 	# there should only be three arguments
 	if (len(sys.argv) is not 3):
 		exit("Usage: python3 glitch.py <input-imgs-dir> <output-imgs-dir>")
+		
+	# need to load the list of words
+	lexicon = set(open('dict.txt','r').read().lower().split())	
+	
 		
 	# here is where we want to capture some options from the user
 	# or keep it on the CLI and more minimal?? (I like that better)
@@ -73,7 +124,7 @@ def main():
 				
 			# assuming we have valid image...
 			print("glitching", i)
-			glitch(file, imgtype)
+			glitch(file, imgtype, lexicon)
 			
 	# end of main()
 	print("hippo")
